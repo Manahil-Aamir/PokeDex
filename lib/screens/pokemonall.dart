73eb98baa5f30c32ce4fd1060fc1ad0772911233
Manahil Aamir <112062135+Manahil-Aamir@users.noreply.github.com>
2024-04-08 12:47:04 +0500

    detail part done

--- lib/screens/pokemonall.dart
+++ lib/screens/pokemonall.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/providers/pokemonprovider.dart';
+import 'package:pokedex/screens/pokemonsingle.dart';
 import 'package:provider/provider.dart';
 import 'package:pokedex/pokemoncolours.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -33,7 +34,7 @@ class _PokemonAllState extends State<PokemonAll> {
             Align(
               alignment: Alignment.topLeft,
               child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                 child: Text('Pokedex',
                 style: TextStyle(
                   color: Colors.grey[700]!,
@@ -59,58 +60,66 @@ class _PokemonAllState extends State<PokemonAll> {
                     ),
                     itemBuilder: (context, index) {
                       final pokemon = pokemons[index];
-                      return Card(
-                        color: PokemonColours.getColorForType(pokemon.type[0]),
-                        child: Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: CachedNetworkImage(
-                              imageUrl: pokemon.img,
-                              height: 100,
-                              width: 100,
-                              placeholder: (context, url) => const CircularProgressIndicator(),
-                              errorWidget: (context, url, error) => const Icon(Icons.error),
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PokemonSingle(pokemon: pokemon,)),
+                          );
+                        },
+                        child: Card(
+                          color: PokemonColours.getColorForType(pokemon.type[0]),
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: CachedNetworkImage(
+                                imageUrl: pokemon.img,
+                                height: 100,
+                                width: 100,
+                                placeholder: (context, url) => const CircularProgressIndicator(),
+                                errorWidget: (context, url, error) => const Icon(Icons.error),
+                                ),
                               ),
-                            ),
-                            const SizedBox(width: 60.0,),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const SizedBox(height: 30),
-                                  Text(
-                                    pokemon.name,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                    ),
-                                  ),
-                                  Container(
-                                    decoration: ShapeDecoration(
-                                      color: Colors.transparent.withOpacity(0.43),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
+                              const SizedBox(width: 60.0,),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(height: 30),
+                                    Text(
+                                      pokemon.name,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w800,
+                                        color: Colors.white,
+                                        fontSize: 19,
                                       ),
                                     ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.fromLTRB(9, 5, 9, 5),
-                                      child: Text(
-                                        pokemon.type[0],
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 15,
+                                    Container(
+                                      decoration: ShapeDecoration(
+                                        color: Colors.transparent.withOpacity(0.43),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(20),
+                                        ),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.fromLTRB(9, 5, 9, 5),
+                                        child: Text(
+                                          pokemon.type[0],
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       );
                     },
---
+++ lib/screens/pokemonsingle.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/models/pokemon.dart';
+
+import '../pokemoncolours.dart';
+
+class PokemonSingle extends StatefulWidget {
+  const PokemonSingle({super.key, required this.pokemon,});
+
+  final Pokemon pokemon;
+  @override
+  State<PokemonSingle> createState() => _PokemonSingleState();
+}
+
+Widget titleText(String title){
+  return Column(
+    children: [
+      Text(
+            title,
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+            ),
+          ),
+      const SizedBox(height: 13),
+    ],
+  );
+}
+
+Widget infoText(String value){
+  return Column(
+    children: [
+      Text(
+            value,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+            ),
+          ),
+      const SizedBox(height: 13),
+    ],
+  );
+}
+
+Widget details(String title, String value) {
+  return Row(
+    children: [
+      Text(
+        title,
+        style: TextStyle(
+          color: Colors.grey[600],
+          fontWeight: FontWeight.w400,
+          fontSize: 18,
+        ),
+      ),
+      const SizedBox(width: 90),
+      Text(
+        value,
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+          fontSize: 18,
+        ),
+      ),
+    ],
+  );
+}
+
+
+class _PokemonSingleState extends State<PokemonSingle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PokemonColours.getColorForType(widget.pokemon.type[0]),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.white, // Comma was added here
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: PokemonColours.getColorForType(widget.pokemon.type[0]),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+        child: Stack(
+          children: [
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  height: MediaQuery.of(context).size.height * 0.55,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                ), 
+                color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(27, 80, 0, 0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            titleText('Name'),
+                            titleText('Height'),
+                            titleText('Weight'),
+                            titleText('Spawn Time'),
+                            titleText('Weakness'),
+                            titleText('Pre Evolution'),
+                            titleText('Next Evolution'),
+                          ],
+                        ),
+                        const SizedBox(width: 6),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            infoText(widget.pokemon.name),
+                            infoText(widget.pokemon.height),
+                            infoText(widget.pokemon.weight),
+                            infoText(widget.pokemon.spawnTime),
+                            infoText(widget.pokemon.weaknesses.join(', ')),
+                            infoText(widget.pokemon.prevEvolution.isNotEmpty? widget.pokemon.prevEvolution.map((e) => e.name).join(' ') : 'Just Hatched'),
+                            infoText(widget.pokemon.nextEvolution.isNotEmpty ? widget.pokemon.nextEvolution.map((e) => e.name).join(' ') : 'Maxed Out'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
